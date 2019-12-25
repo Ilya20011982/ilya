5a951d1864b552cf7d0742b366ffc701be4e8bd8
Ilya20011982 <ilya_fr@mail.ru>
2019-12-25 10:06:40 +0500

    Create 68.2 задача заполнение матрицы числами.py

---
+++ 68.2 задача заполнение матрицы числами.py
@@ -0,0 +1,43 @@
+# 68. *Заполните матрицу, содержащую N строк и M столбцов, натуральными числами по 
+# спирали и змейкой:
+# Вариант "A"
+def print_matrix(matrix, lenght=5):
+    for line in matrix:
+        for item in line:
+            print(f'{item:{lenght}}', end=' ')
+        print()
+
+
+lines, rows = map(int, input('Введите количество строк и столбцов: ').split())
+matrix = [[0 for i in range(rows)] for j in range(lines)]
+x = 1
+line = 0
+row = 0
+i, j = 0, 0
+while matrix:
+    
+    for row in range(rows - 1):
+        matrix[line][row] = x
+        x += 1
+
+    for line in range(lines - 1):
+        matrix[line][rows - 1] = x
+        x += 1
+
+    for row in range(rows - 1):
+        matrix[lines - 1][-row - 1] = x
+        x += 1
+        
+    for line in range(lines - 1):
+        matrix[-line - 1][row - row] = x
+        x += 1
+    
+    lines -= 1
+    rows -= 1
+    
+
+
+
+print('matrix')
+print_matrix(matrix)
+print('line', line, 'row', row)
