e1c0f361d59f170bb48da38da245c0ffd9f94b54
Anatoly Chistov <atchistov@gmail.com>
2020-02-16 15:15:37 +0300

    Задача с заполнением матрица

--- 68.2.3 задача заполнение матрицы числами.py
+++ 68.2.3 задача заполнение матрицы числами.py
@@ -1,15 +1,16 @@
+def compare(xy):
+    x, y = xy
+    return (x + y, -y if (x + y) % 2 else y)
+
 def zigzag(n):
     '''zigzag rows'''
-    def compare(xy):
-        x, y = xy
-        return (x + y, -y if (x + y) % 2 else y)
     xs = range(n)
     return {index: n for n, index in enumerate(sorted(
         ((x, y) for x in xs for y in xs),
         key=compare
     ))}
- 
- 
+
+
 def printzz(myarray):
     '''show zigzag rows as lines'''
     n = int(len(myarray) ** 0.5 + 0.5)
@@ -17,6 +18,7 @@ def printzz(myarray):
     print('\n'.join(
         [''.join("%3i" % myarray[(x, y)] for x in xs) for y in xs]
     ))
- 
- 
-printzz(zigzag(6))
+
+x = zigzag(6)
+print(x)
+printzz(x)
