a257195048e5e84694e957f84d87bbad8369e6ab
Ilya20011982 <ilya_fr@mail.ru>
2020-02-18 19:22:00 +0500

    Update задача со stepic 3.4 на Файловый ввод вывод.py

--- задача со stepic 3.4 на Файловый ввод вывод.py
+++ задача со stepic 3.4 на Файловый ввод вывод.py
@@ -1,21 +1,21 @@
-# Напишите программу, которая считывает из файла строку, соответствующую тексту, 
+# Напишите программу, которая считывает из файла строку, соответствующую тексту,
 # сжатому с помощью кодирования повторов, и производит обратную операцию, получая исходный текст.
 # Запишите полученный текст в файл и прикрепите его, как ответ на это задание.
 with open('in.txt', encoding='utf-8') as inf:
-    text = inf.readlines()
-    text_out = ''
-    number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    ch = ''
-    num = ''
-    for line in text:
-        for j in range(len(line) - 2):
-            if line[j] not in number:
-                ch = line[j]
-            if line[j + 1] in number:
-                num += line[j + 1]
-            if line[j + 2] not in number:
-                text_out += ch * int(num)
-                ch = ''
-                num = ''
+    text = inf.read()
+
+text_out = ''
+ch = ''
+num = ''
+for j in range(len(text) - 2):
+    if not text[j].isdigit():
+        ch = text[j]
+    if text[j + 1].isdigit():
+        num += text[j + 1]
+    else:
+        text_out += ch * (int(num) if num else 1)
+        ch = ''
+        num = ''
+
 with open('out.txt', 'w', encoding='utf-8') as inf:
-    inf.write(''. join(text_out))
+    inf.write(text_out)
