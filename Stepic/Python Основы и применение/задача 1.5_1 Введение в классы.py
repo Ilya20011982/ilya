7a0a749248eeade031bce4a6c41ce34c6b4be858
Ilya20011982 <ilya_fr@mail.ru>
2020-02-23 16:30:14 +0500

    Create задача 1.5_1 Введение в классы.py

---
+++ Stepic/Python Основы и применение/задача 1.5_1 Введение в классы.py
@@ -0,0 +1,43 @@
+# Реализуйте класс MoneyBox, для работы с виртуальной копилкой.
+# Каждая копилка имеет ограниченную вместимость, которая выражается целым числом – количеством монет, 
+# которые можно положить в копилку. Класс должен поддерживать информацию о количестве монет в копилке, 
+# предоставлять возможность добавлять монеты в копилку и узнавать, можно ли добавить в копилку ещё какое-то количество монет, 
+# не превышая ее вместимость.
+# Класс должен иметь следующий вид
+# class MoneyBox:
+#     def __init__(self, capacity):
+#         # конструктор с аргументом – вместимость копилки
+
+#     def can_add(self, v):
+#         # True, если можно добавить v монет, False иначе
+
+#     def add(self, v):
+#         # положить v монет в копилку
+# При создании копилки, число монет в ней равно 0.
+# Примечание: Гарантируется, что метод add(self, v) будет вызываться только если can_add(self, v) – True﻿.
+
+class MoneyBox:
+    def __init__(self, capacity):
+        self.capacity = capacity 
+    def can_add(self, v):
+        self.v = v
+        if self.capacity - self.v >= 0: 
+            return True
+        else:
+            return False
+    def add(self, v):
+        if MoneyBox.can_add(self, v) == True:
+            self.capacity -= v
+            return self.capacity
+        else:
+            return False
+
+x = MoneyBox(15)
+y = MoneyBox(60)
+print(x.add(5))
+print(x.add(9))
+print(x.add(3))
+print()
+print(y.add(25))
+print(y.add(9))
+print(y.add(13))
