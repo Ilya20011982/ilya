ebf2116e88e72c30e6fb35bf6c7d2ba363c76257
Ilya20011982 <ilya_fr@mail.ru>
2020-05-05 23:29:36 +0500

    Update 1.4 Использование BeautifulSoup_1.py

--- Stepic/Python для решения практических задач/1.4 Использование BeautifulSoup_1.py
+++ Stepic/Python для решения практических задач/1.4 Использование BeautifulSoup_1.py
@@ -4,3 +4,10 @@
 from urllib.request import urlopen, urlretrieve
 from bs4 import BeautifulSoup
 
+resp = urlopen('https://stepik.org/media/attachments/lesson/209723/3.html') # скачиваем файл
+html = resp.read().decode('utf8') # считываем содержимое
+soup = BeautifulSoup(html, 'html.parser') # делаем суп
+summ = 0
+for tr in soup.find_all('td'):
+    summ += int(tr.text)
+print(summ)
